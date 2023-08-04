5c0d9a27bcd192e26ad2c19fb3e33c4076312388
alpgiray <alpgirayacar@outlook.com>
2023-08-04 12:57:07 +0300

    http get resbonse metods added

---
+++ MVCExample/Controllers/PeopleController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCExample.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace MVCExample.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+           List<Person> PersonList = new List<Person>();
+
+            using(var httpClient = new HttpClient())
+            {   
+                
+                using(var response = await httpClient.GetAsync("https://localhost:7299/Home"))
+                {
+                    var gelenString = await response.Content.ReadAsStringAsync();
+                    PersonList = JsonConvert.DeserializeObject<List<Person>>(gelenString);
+
+                }
+            }
+            
+            return View(PersonList);
+        }
+
+       
+
+
+   
+
+
+        
+
+        
+
+
+    }
+}
+
